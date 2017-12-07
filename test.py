82f34ce33397719895b74224008e90ae9ed91ef3
Felix Wolff <felix.wolff2@student.hpi.de>
2017-12-07 21:17:14 +0100

    Yeah

--- test.py
+++ test.py
@@ -3,15 +3,28 @@
 import platform
 
 # check for linux distribution
-distro = platform.linux_distribution()[0]
+distro = platform.linux_distribution()
+package_list = []
 
 # depending on distro load appropriate package for interfacing
 # with package management mechanism
-if distro == "arch":
-    import pacman
-    
-    package_list = pacman.get_installed()
-    # for each package in package_list:
-    # gather real package name
-    print(package_list)
+if distro[0] == "arch":
+  import pacman
+
+  package_list = pacman.get_installed()
+  # for each package in package_list:
+  # gather real package name
+  print(package_list)
+elif distro[0] == 'debian':
+  import apt
+  cache = apt.Cache()
+
+  for pkg in apt.Cache():
+    if cache[pkg.name].is_installed:
+      package_list.append({
+        "id": pkg.id,
+        "version": pkg.installed.version
+      })
+
 # do entity matching here
+print(package_list)
