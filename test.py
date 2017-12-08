15590d119a4589f2f4c26a692c1bfea6ef170954
Felix Wolff <felix.wolff2@student.hpi.de>
2017-12-08 14:35:50 +0000

    Found out that fulltext_search matches too broadly and that description texts often contain references to other softwares

--- test.py
+++ test.py
@@ -3,6 +3,8 @@
 import platform
 import requests
 from subprocess import check_output
+from cpe import CPE
+from pprint import pprint
 
 # check for linux distribution
 distro = platform.linux_distribution()
@@ -29,19 +31,35 @@ elif distro[0] == 'debian':
         "version": pkg.installed.version
       })
 
-# do entity matching here
+package_list = [{
+    "name": "gcc",
+    "version": "7.2.0-3"
+}]
+pkg_cves = dict()
 
-for pkg in package_list[0:50]:
-  out = check_output(["/mnt/brick/home/fwolff/cve-search-master/bin/search_fulltext.py", "-q", pkg['name']]).splitlines()
+for pkg in package_list:
+    #out = check_output(["/mnt/brick/home/fwolff/cve-search-master/bin/search_fulltext.py", "-q", pkg['name']]).splitlines()
+    out = check_output(["/home/felix/cve-search/bin/search_fulltext.py", "-q", pkg['name']]).splitlines()
+    out = [ o.decode("utf-8") for o in out ]
 
-  # acquire CVE information from database for every CVE
+  # acquire CVE information from cve-search API for every CVE
   cve_infos = []
   for cve in out:
-    cveUrl = "http://127.0.0.1:5000/api/cve/{0}".format(cve)
-    cveJson = requests.get(requestUrl).json()
-    cve_infos.append(cveJson)
+      cveUrl = "http://127.0.0.1:5000/api/cve/{0}".format(cve)
+      cveJson = requests.get(cveUrl).json()
+
+    if cveJson != None:
+        cve_infos.append(dict(cveJson))
+
+  if len(cve_infos) > 0:
+      print(pkg['name'], "matches these CPEs:")
 
   # compare CVE critical version numbers with local version number
-
-  print(pkg['name'], out)
-
+  for cve in cve_infos:
+      try:
+          cpe_str = cve["vulnerable_configuration_cpe_2_2"][0]
+          cve_cpe = CPE(cpe_str)
+          print("\t",cve_cpe)
+      except:
+          print("bad cve_cpe")
+          # it can happen that the vulnerable configuration array is empty!
