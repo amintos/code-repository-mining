f7e13fc2f9047ee3cd8d6342fa8531b942ae624e
Felix Wolff <felix.wolff2@student.hpi.de>
2018-01-08 17:37:53 +0100

    Amend URL regexes in materialized views

--- create-tweet-extracted-views.sql
+++ create-tweet-extracted-views.sql
@@ -8,6 +8,6 @@ WITH DATA;
 CREATE MATERIALIZED VIEW view_cve_referring_tweets_extracted_domains AS
 SELECT
   id AS tweet_id,
-  regexp_matches(text, 'http[s]:\/\/?\ ?([-a-zA-Z0-9:%._\+~#=]{2,255}\.[a-z]{2,6})', 'g') AS domain
+  regexp_matches(text, 'http[s]{0,1}:\/\/?\ ?([-a-zA-Z0-9:%._\+~#=]{2,255}\.[a-z]{2,6})', 'g') AS domain
 FROM cve_referring_tweets
 WITH DATA;
