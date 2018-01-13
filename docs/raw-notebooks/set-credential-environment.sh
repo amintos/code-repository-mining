acafc5257df9fe19bf3615fe25fde2949e7bfc15
Felix Wolff <felix.wolff2@student.hpi.de>
2018-01-13 12:17:34 +0100

    Hello credentials for jupyter

---
+++ docs/raw-notebooks/set-credential-environment.sh
@@ -0,0 +1,9 @@
+export CRM_DB_NAME=..
+export CRM_DB_USER=..
+export CRM_DB_PW=..
+export MONGODB_URL=..
+
+export MONGODB_NAME=...
+export MONGODB_USER=..
+export MONGODB_PW=..
+export MONGODB_URL=..
