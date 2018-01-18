1721f5c0b23f9ebdbde907018a294d0885688ce3
Felix Wolff <felix.wolff2@student.hpi.de>
2018-01-18 10:57:51 +0000

    Play around with nbconvert templates

--- docs/nbconvert_config.py
+++ docs/nbconvert_config.py
@@ -3,4 +3,4 @@ c = get_config()
 #Export all the notebooks in the current directory to the sphinx_howto format.
 c.NbConvertApp.notebooks = ['raw-notebooks/Analysis of Twitter and CVE reference data.ipynb']
 c.NbConvertApp.export_format = 'html'
-c.Exporter.template_file = 'md_cell_list'
+c.Exporter.template_file = 'full_custom'
