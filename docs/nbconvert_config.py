2c6f4f8bb6877ca9d1ccd0a302a65fe053fd0bfb
Felix Wolff <felix.wolff2@student.hpi.de>
2018-02-05 16:41:41 +0000

    Set up styling and start documentation

--- docs/nbconvert_config.py
+++ docs/nbconvert_config.py
@@ -1,6 +1,6 @@
 c = get_config()
 
 #Export all the notebooks in the current directory to the sphinx_howto format.
-c.NbConvertApp.notebooks = ['index.pynb']
+c.NbConvertApp.notebooks = ['index.ipynb']
 c.NbConvertApp.export_format = 'html'
 c.Exporter.template_file = 'full_custom'
